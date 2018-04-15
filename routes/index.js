26801d3974bf8488ee204df93b2d044cb1f87252
Nilansh Bansal <bansalnilansh@gmail.com>
2018-04-15 13:15:49 +0530

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb').MongoClient;
 var assert = require('assert');
-var url = 'mongodb://localhost:27017/test';
+var url = 'mongodb://localhost:27017';
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'URL Shortener' });
